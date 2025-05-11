966f985616873d73f8ee1ea47a6c10707acff45d
faysal abir <faysalabir125@gmail.com>
2025-05-12 00:22:46 +0600

    componets create for educator

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,7 @@ import AddCourse from './pages/educator/AddCourse'
 import MyCourses from './pages/educator/MyCourses'
 import StudentEnrolled from './pages/educator/StudentEnrolled'
 import Navbar from './components/student/Navbar'
+import "quill/dist/quill.snow.css"
 
 
 const App = () => {
--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-
+import Navbar from '../../components/educator/Navbar'
+import SideBar from '../../components/educator/Sidebar'
+import Footer from '../../components/educator/Footer'
 const Educator = () => {
   return (
-    <div>
-      <h1>Educator Page</h1>
-      <div>
+    <div className='text-default bg-white min-h-screen'>
+       <Navbar />
+      <div className='flex'>
+        <SideBar />
+        <div>
         {<Outlet/>}
+        </div>
       </div>
+      <Footer />
     </div>
   )
 }
