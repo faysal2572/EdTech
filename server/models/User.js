60d6bd2220aaceb849177323a53bf8b14c70b07b
faysal abir <faysalabir125@gmail.com>
2025-05-13 01:04:32 +0600

    vercel connnection

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,20 @@
+import express from 'express';
 import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    _id: {type: String,required: true,},
+    name: {type: String,required: true,},
+    email: {type: String,required: true,unique: true,},
+    imgUrl: {type: String,required: true,},
+    enrollCourses:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Course'
+        }
+    ]
+  },
+    {timestamps: true,});
+
+const User = mongoose.model('User', userSchema);
+export default User;    
