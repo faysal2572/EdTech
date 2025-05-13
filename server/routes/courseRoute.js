c1dfcc76a7493632dbdf4dfe53c4de50d69ff6ac
faysal abir <faysalabir125@gmail.com>
2025-05-13 21:38:32 +0600

    course route

--- server/routes/courseRoute.js
+++ server/routes/courseRoute.js
@@ -1,5 +1,14 @@
-import express from 'express';
+import express from 'express'
+import { getAllCourse, getCourseId } from '../controllers/courseController.js';
 
-const courseRouter = express.Router();
+
+const courseRouter = express.Router()
+
+// Get All Course
+courseRouter.get('/all', getAllCourse)
+
+// Get Course Data By Id
+courseRouter.get('/:id', getCourseId)
+
 
 export default courseRouter;
