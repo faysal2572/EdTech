220847999a29a767a48ff4714d3c79074c51956f
sarahhh-hhh <sarah.alom3009@gmail.com>
2025-05-14 15:26:59 +0600

    course controlller

--- server/server.js
+++ server/server.js
@@ -36,3 +36,6 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 })
+
+app.use('/api/course', express.json(), courseRouter)
+import courseRouter from './routes/courseRoute.js'
